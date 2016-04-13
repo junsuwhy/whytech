839918eecf06bd15e93b0555fc1797ed7dde56c9
Chang Shu-huai <junsuwhy@gmail.com>
2016-04-13 11:12:34 +0800

    Create share_to_fb.js

---
+++ web/share_to_fb/share_to_fb.js
@@ -0,0 +1,18 @@
+//FB share, used in PC
+function shareFacebook(url, options, app_id){
+    /**
+     * options: 
+     * picture, name, description, redirect_uri
+     */
+
+	var share_url = 'https://www.facebook.com/dialog/feed?app_id=' + app_id + '&display=popup&picture=' + encodeURI(options.picture) + '&link=' + url + '&redirect_uri=' + encodeURI(options.redirect_uri) + '&name=' + encodeURI(options.name) + '&description=' + encodeURI(options.description);
+
+	var w_width=600;                                                                        
+	var w_height=300;                                                                        
+	var x=(screen.width-w_width)/2;                                                        
+	var y=(screen.height-w_height)/2;                                                     
+	var ww='width='+w_width+',height='+w_height+',top='+y+',left='+x;   
+	
+	window.open(share_url, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,'+ww);
+	
+}
